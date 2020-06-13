2e7c60d158c4b694a1f1203512e8d66aabe09a53
jpedrosiq <johnpedrosiqueira@gmail.com>
2020-06-12 18:29:24 -0700

    finish song notes

--- src/song.js
+++ src/song.js
@@ -1,17 +1,12 @@
 export const beatsPerMeasure = 8;
 
-import songIntro from './songs/song_intro';
-import songVerse1 from './songs/song_verse_1';
-import songChorus1 from './songs/song_chorus_1';
-import songSolo1 from './songs/song_solo_1';
-import songVerse2 from './songs/song_verse_2';
-import songChorus2 from './songs/song_chorus_2';
-import songSolo2 from './songs/song_solo_2';
-import songBridge from './songs/song_bridge';
-
+import songIntro from "./songs/song_intro";
+import songVerse from "./songs/song_verse_1";
+import songChorus from "./songs/song_chorus_1";
+import songSolo from "./songs/song_solo_1";
 
 export const songNotes = songIntro.concat(
-    songVerse1,
-    songChorus1,
-    songSolo1,
+    songVerse,
+    songChorus,
+    songSolo
 );
